36614f4a3ed69df2f979c1c98daf059f5d8f196e
Diego Perez <dissit20@gmail.com>
2021-12-23 11:03:02 +0100

    Rama Diego

---
+++ CustomVector.h
@@ -0,0 +1,13 @@
+#pragma once
+class CustomVector
+{
+	int size = 0;
+	int capacity = 0;
+	int* values = nullptr;
+public:
+	~CustomVector();
+	int Length();
+	void Add(int value);
+	void Remove();
+	int At(int index);
+};
---
+++ Player.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ConsoleUtils.h"
+#include "Map.h"
+#include "Enemy.h"
+#include <vector>
+enum USER_INPUTS { NONE, UP, DOWN, RIGHT, LEFT, QUIT };
+class Player
+{
+	COORD spawn;
+	COORD direction;
+	char character;
+	ConsoleUtils::CONSOLE_COLOR background;
+	ConsoleUtils::CONSOLE_COLOR foreground;
+public:
+	COORD position;
+	int points = 0;
+	Player(COORD _spawn);
+	void Update(Map* _map, USER_INPUTS input, std::vector<Enemy>* enemies);
+	void Draw();
+
+};
